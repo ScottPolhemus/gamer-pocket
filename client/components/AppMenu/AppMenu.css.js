f1042f451e7f7be3a0553782d9df487fd78073b6
Scott Polhemus <scott@polhem.us>
2021-07-27 17:12:32 -0400

    button styling

--- client/components/AppMenu/AppMenu.css.js
+++ client/components/AppMenu/AppMenu.css.js
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components'
 
-import { buttonShadow } from '../../styles/color'
+import { buttonShadow, color } from '../../styles/color'
 
 export const MenuContainer = styled.div`
   flex: 1;
@@ -34,7 +34,7 @@ export const MenuTitle = styled.h2`
   color: white;
 `
 
-const MenuInput = styled.input`
+const menuInputBase = css`
   appearance: none;
   width: 100%;
   padding: 0.5rem;
@@ -42,12 +42,54 @@ const MenuInput = styled.input`
   font-size: 1.25rem;
   border: 0;
   border-radius: 4px;
+`
+
+const MenuInput = styled.input`
+  ${menuInputBase}
   ${buttonShadow('white')}
 `
 
-export const MenuButton = styled(MenuInput).attrs({
-  as: 'button'
-})``
+export const MenuButton = styled.button`
+  ${menuInputBase}
+  position: relative;
+  ${buttonShadow('black')}
+  background: linear-gradient(
+    to top,
+    ${color('lightgray').darken(0.25).hsl().string()} 25%,
+    ${color('lightgray').hsl().string()}
+  );
+  border: 1px solid gray;
+  ${'' /* border-bottom-width: 3px; */}
+  border-left-color: gray;
+  border-top-color: gray;
+  color: hsl(0,0%,40%);
+  text-shadow: -1px -1px 0 hsla(0,0%,5%,0.25);
+
+  &::before {
+    display: none;
+    content: '';
+    position: absolute;
+    top: -1px;
+    bottom: -3px;
+    left: -1px;
+    right: -1px;
+    background: gray;
+    border: 1px solid transparent;
+    border-bottom: 3px solid gray;
+    border-radius: inherit;
+    z-index: -1;
+  }
+
+  &:active {
+    box-shadow: none;
+    box-shadow: 0 0 1rem ${color('black').alpha(0.25).hsl().string()};
+    transform: scale(0.99);
+
+    &:before {
+      display: none;
+    }
+  }
+`
 
 export const MenuSelect = styled(MenuInput).attrs({
   as: `select`
@@ -120,7 +162,7 @@ export const ToggleText = styled.span`
   margin: 0 0.5rem;
   font-family: sans-serif;
   vertical-align: middle;
-  color: white;
+  color: hsla(0,0%,100%,0.5);
 `
 
 export const SettingsLink = styled.a`
